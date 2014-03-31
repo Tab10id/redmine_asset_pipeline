a643c15594faff30f78ec8c638eed2010a315abd
Tab Loid <tabloidmeister@gmail.com>
2014-03-31 11:20:39 +0400

    fix ApplicationHelper patching. rename some files. change tabs to spaces

--- lib/redmine_plugin_asset_pipeline.rb
+++ lib/redmine_plugin_asset_pipeline.rb
@@ -12,16 +12,20 @@ module RedminePluginAssetPipeline
     initializer 'redmine.asset_pipeline', :before => 'sprockets.environment' do
       RedmineApp::Application.configure do
         config.assets.enabled = true
-	config.assets.prefix = '' # Redmine save all assets in root of public =(
-	config.assets.css_compressor = :yui
-	config.assets.js_compressor = :yui
-	config.assets.paths << "#{config.root}/public/plugin_assets"
-	
-	# TODO: delete environment hardcode
-	config.assets.debug = Rails.env.eql?("production") ? false : true
-	config.assets.compile = Rails.env.eql?("production") ? false : true
-	config.assets.compress = Rails.env.eql?("production") ? true : false
+        config.assets.prefix = '' # Redmine save all assets in root of public =(
+        config.assets.css_compressor = :yui
+        config.assets.js_compressor = :yui
+        config.assets.paths << "#{config.root}/public/plugin_assets"
+        
+        # TODO: delete environment hardcode
+        config.assets.debug = Rails.env.eql?("production") ? false : true
+        config.assets.compile = Rails.env.eql?("production") ? false : true
+        config.assets.compress = Rails.env.eql?("production") ? true : false
       end
+    end
+
+    config.to_prepare do
+      require_dependency 'redmine_plugin_asset_pipeline/application_helper_patch'
     end
   end
 end
--- lib/redmine_plugin_asset_pipeline/application_helper_patch.rb
+++ lib/redmine_plugin_asset_pipeline/application_helper_patch.rb
@@ -4,11 +4,11 @@ module ApplicationHelper
     plugin = options.delete(:plugin)
     sources = sources.map do |source|
       if plugin && plugin != 'application'
-	"/#{plugin}/stylesheets/#{source}"
+        "/#{plugin}/stylesheets/#{source}"
       elsif current_theme && current_theme.stylesheets.include?(source)
-	current_theme.stylesheet_path(source)
+        current_theme.stylesheet_path(source)
       else
-	"/stylesheets/#{source}"
+        "/stylesheets/#{source}"
       end
     end
     super *sources, options
@@ -19,9 +19,9 @@ module ApplicationHelper
     plugin = options.delete(:plugin)
     sources = sources.map do |source|
       if plugin && plugin != 'application'
-	"/#{plugin}/javascripts/#{source}"
+        "/#{plugin}/javascripts/#{source}"
       else
-	"/javascripts/#{source}"
+        "/javascripts/#{source}"
       end
     end
     super *sources, options
