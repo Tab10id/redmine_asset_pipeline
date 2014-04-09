30b3e572797b557c147c372e9f4826510737d5d0
Tab Loid <tabloidmeister@gmail.com>
2014-04-09 18:25:11 +0400

    patch for include_calendar_headers_tags (remove first '/' in path_to_image)

--- lib/redmine_plugin_asset_pipeline/application_helper_patch.rb
+++ lib/redmine_plugin_asset_pipeline/application_helper_patch.rb
@@ -43,4 +43,28 @@ module ApplicationHelper
     end
     super source, options
   end
+
+  def include_calendar_headers_tags
+    unless @calendar_headers_tags_included
+      @calendar_headers_tags_included = true
+      content_for :header_tags do
+        start_of_week = Setting.start_of_week
+        start_of_week = l(:general_first_day_of_week, :default => '1') if start_of_week.blank?
+        # Redmine uses 1..7 (monday..sunday) in settings and locales
+        # JQuery uses 0..6 (sunday..saturday), 7 needs to be changed to 0
+        start_of_week = start_of_week.to_i % 7
+
+        tags = javascript_tag(
+            "var datepickerOptions={dateFormat: 'yy-mm-dd', firstDay: #{start_of_week}, " +
+                "showOn: 'button', buttonImageOnly: true, buttonImage: '" +
+                path_to_image('images/calendar.png') +
+                "', showButtonPanel: true, showWeek: true, showOtherMonths: true, selectOtherMonths: true};")
+        jquery_locale = l('jquery.locale', :default => current_language.to_s)
+        unless jquery_locale == 'en'
+          tags << javascript_include_tag("i18n/jquery.ui.datepicker-#{jquery_locale}.js")
+        end
+        tags
+      end
+    end
+  end
 end
